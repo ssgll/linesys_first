fa6c2d4f22a0696f49fa231298c876a812748cf1
ssgll <guo.orm.o@gmail.com>
2020-03-14 18:30:29 +0800

    linesys3

--- app/urls.py
+++ app/urls.py
@@ -3,4 +3,4 @@ from app import index
 from app.views import pageIndex
 
 
-index.add_url_rule(rule="/index/", endpoint=None, view_func=pageIndex, methods=["POST", "GET"])
+index.add_url_rule(rule="/", endpoint=None, view_func=pageIndex, methods=["POST", "GET"])
